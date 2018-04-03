ac04b2f8458bdee9097db761116711579f5af937
indispo <fdemech@gmail.com>
2018-04-03 16:36:57 +0200

    zezaeaze

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<footer class="main-footer">
+    <p class="copyright">&copy équipe_Florent</p>
+</footer>
+</div>
+
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Mina|Oswald" rel="stylesheet">    
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Bienvenue sur mon blog</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header class="main-header">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="main-content">
