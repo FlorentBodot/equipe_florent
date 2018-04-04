6b614faf48d1dfb0b701e4292697c81e3a1631d3
Benjamin Cahot <benjamincahot@icloud.com>
2018-04-04 11:38:27 +0200

    update
    ajout de bootstrap dans l'header

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-    <h1>COUCOU LES COUPINES</h1>
+include('inc/header.php');
 
-    <h2>coucou benjamin</h2>
-
-</body>
-</html>
+?>
